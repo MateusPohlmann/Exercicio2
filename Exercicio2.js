265546a88414e8a1b5fc7d1d1febd8a367f1f9f4
MateusPohlmann <mplauxen@gmail.com>
2024-08-29 16:27:48 -0300

    Add files via upload

---
+++ Exercicio2.js
@@ -0,0 +1,19 @@
+//1.Crie uma variavel que receba e armazene a sua idade no tipo number
+
+let idade = 15
+
+//2.Crie uma outra variavel que receba o valor da sua primeira variavel com o seu tipo alterado para String
+
+let idadeTexto = idade.toString()
+
+//3.imprima a duas mensagens no terminal
+
+console.log("Minha idade é" , idade , "e é uma variavel do tipo" , typeof idade , "ou do tipo" , typeof idadeTexto)
+
+
+
+
+
+
+
+
