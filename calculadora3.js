d1e8ad1fe5d6de43c863c134830aa6560f4a4356
MateusPohlmann <mplauxen@gmail.com>
2024-09-25 14:20:47 -0300

    Update calculadora3.js

--- calculadora3.js
+++ calculadora3.js
@@ -17,7 +17,7 @@ function multiplicar(num1, num2) {
     let resultado = num1 * num2
     return resultado
 }
-function dividir(num1, num2) {
+function dividir(num1, num2) {                                
     let resultado = num1 / num2
     return resultado
 }
@@ -35,99 +35,100 @@ while (querContinuar) {                                                      //i
         numero2 = Number(ask.question(`Qual o segundo numero: `))
     }
     console.clear()
-    let operacao = Number(ask.question(`Qual sera a operacao?    
------------------------------------------------------
+    let operacao = Number(ask.question(`
 Seus numeros sao: ${numero1} e ${numero2}
-Digite:
-1 - Soma
-2 - Subtracao
-3 - Multiplicacao
-4 - Dividir
-5 - Calculo de mod
-6 - Reinserir novos numeros
-0 - Sair
+Qual sera a operacao?    
+-----------------------------------------------------
+Soma                    - 1
+Subtracao               - 2
+Multiplicacao           - 3
+Dividir                 - 4
+Calculo de mod          - 5
+Reinserir novos numeros - 6
+Sair                    - 0
 -----------------------------------------------------
 Resposta: `))
     console.clear()
     switch (operacao) {                                                               //switch do menu
         case 1:                                                                       //soma
             console.log(`${numero1} + ${numero2} = ${somar(numero1, numero2)}`)
+            console.log(`
+-----------------------------------------------------
+Seus numeros sao: ${numero1} e ${numero2}
+-----------------------------------------------------
+            `)
             usarOsMesmosNume = ask.question(`Quer usar os mesmo numeros (Sim ou Nao): `)
             if (usarOsMesmosNume.toUpperCase() === `SIM`) {
                 num1Cop = numero1
                 num2Cop = numero2
                 usarOsMesmosNume = true
-                console.log(`
--------------------------------------------
-Seus numeros sao: ${num1Cop} e ${num2Cop}
--------------------------------------------
-                    `)
             }
             break
         case 2:                                                                           //subtração
             console.log(`${numero1} - ${numero2} = ${subtrair(numero1, numero2)}`)
+            console.log(`
+-----------------------------------------------------
+Seus numeros sao: ${numero1} e ${numero2}
+-----------------------------------------------------
+            `)
             usarOsMesmosNume = ask.question(`Quer usar os mesmo numeros (Sim ou Nao): `)
             if (usarOsMesmosNume.toUpperCase() === `SIM`) {
                 num1Cop = numero1
                 num2Cop = numero2
                 usarOsMesmosNume = true
-                console.log(`
--------------------------------------------
-Seus numeros sao: ${num1Cop} e ${num2Cop}
--------------------------------------------
-                    `)
             }
             break
-        case 3:
+        case 3:                                                                            //multiplicacao
             console.log(`${numero1} * ${numero2} = ${multiplicar(numero1, numero2)}`)
+            console.log(`
+-----------------------------------------------------
+Seus numeros sao: ${numero1} e ${numero2}
+-----------------------------------------------------
+            `)
             usarOsMesmosNume = ask.question(`Quer usar os mesmo numeros (Sim ou Nao): `)
             if (usarOsMesmosNume.toUpperCase() === `SIM`) {
                 num1Cop = numero1
                 num2Cop = numero2
                 usarOsMesmosNume = true
-                console.log(`
--------------------------------------------
-Seus numeros sao: ${num1Cop} e ${num2Cop}
--------------------------------------------
-                    `)
+                
             }
             break
-        case 4:
+        case 4:                                                                           //divisao
             console.log(`${numero1} / ${numero2} = ${dividir(numero1, numero2)}`)
+            console.log(`
+-----------------------------------------------------
+Seus numeros sao: ${numero1} e ${numero2}
+-----------------------------------------------------
+            `)
             usarOsMesmosNume = ask.question(`Quer usar os mesmo numeros (Sim ou Nao): `)
             if (usarOsMesmosNume.toUpperCase() === `SIM`) {
                 num1Cop = numero1
                 num2Cop = numero2
                 usarOsMesmosNume = true
-                console.log(`
--------------------------------------------
-Seus numeros sao: ${num1Cop} e ${num2Cop}
--------------------------------------------
-                    `)
             }
             break
-        case 5:
+        case 5:                                                                           //calculo de mod
             console.log(`${numero1} % ${numero2} = ${mod(numero1, numero2)}`)
+            console.log(`
+-----------------------------------------------------
+Seus numeros sao: ${numero1} e ${numero2}
+-----------------------------------------------------
+            `)
             usarOsMesmosNume = ask.question(`Quer usar os mesmo numeros (Sim ou Nao): `)
             if (usarOsMesmosNume.toUpperCase() === `SIM`) {
                 num1Cop = numero1
                 num2Cop = numero2
                 usarOsMesmosNume = true
-                console.log(`
--------------------------------------------
-Seus numeros sao: ${num1Cop} e ${num2Cop}
--------------------------------------------
-`)
             }
             break
-        case 6:
+        case 6:                                                                            //reinserir os numeros
             querContinuar = true
             usarOsMesmosNume = false
             break
-        case 0:
+        case 0:                                                                           //sair
             querContinuar = false
             break
-        default:
+        default:                                                                          //operacao inesistente
             console.log(`Operacao invalida`)
             break
     }
